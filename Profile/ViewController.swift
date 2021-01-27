b9f54b1748b464457ff7c3464c5dcc5a295fae8f
Yo Higashida <penguin4731@gmail.com>
2021-01-27 18:09:08 +0900

    プロファイルアプリ応用の完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -12,27 +12,62 @@ class ViewController: UIViewController {
     // 画像を表示する変数を作る
     @IBOutlet var profileImageView: UIImageView!
     
+    // ラベルを表示する変数をつくる
+    @IBOutlet var profileLabel: UILabel!
+    
     // ラベルを表示する変数を作る
     @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // 画面を見えない状態にする
-        profileImageView.isHidden = true
-        
-        // ラベルを見えない状態にする
-        profileCommentLabel.isHidden = true
     }
     
+    // ボタンがタップされた時に
     @IBAction func tapButton1() {
-        // 画面を見える状態にする
-        profileImageView.isHidden = false
+        // 画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "philImage")
         
-        // ラベルを見える状態にする
-        profileCommentLabel.isHidden = false
+        // ラベルをLabelに反映する
+        profileLabel.text = "名前"
+        
+        // コメントをLabelに反映する
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
     }
-
+    
+    // ボタンがタップされた時に
+    @IBAction func tapButton2() {
+        // 画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "trackImage")
+        
+        // ラベルをLabelに反映する
+        profileLabel.text = "スポーツ"
+        
+        // コメントをLabelに反映する
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+    }
+    
+    // ボタンがタップされた時に
+    @IBAction func tapButton3() {
+        // 画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "appleImage")
+        
+        // ラベルをLabelに反映する
+        profileLabel.text = "好きな食べ物"
+        
+        // コメントをLabelに反映する
+        profileCommentLabel.text = "りんごが好きで、いつも持ち歩いている"
+    }
+    
+    // ボタンがタップされた時に
+    @IBAction func tapButton4() {
+        // 画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "flightImage")
+        
+        // ラベルをLabelに反映する
+        profileLabel.text = "趣味"
+        
+        // コメントをLabelに反映する
+        profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+    }
 }
 
