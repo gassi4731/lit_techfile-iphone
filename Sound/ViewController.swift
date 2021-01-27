e1eddf741d1fbf765e4df50a8a0bce1cbe2ebb44
Yo Higashida <penguin4731@gmail.com>
2021-01-27 15:57:20 +0900

    add

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // ドラムのサウンドファイルを読み込んで、プレイヤーをつくる
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // ドラムがタップされた時に呼ばれる
+    @IBAction func tapDrumButton() {
+        
+        // ドラムの音を巻き戻す
+        drumSoundPlayer.currentTime = 0
+        
+        // ドラムの音を再生する
+        drumSoundPlayer.play()
+    }
 }
 
